23da544e9e690839c63da95bb2052bcad23c2ead
mqalmesri <66695678+mqalmesri@users.noreply.github.com>
2020-06-13 19:06:50 +0300

    who's mohammed

---
+++ cw1-565-محمد قاسم المسري.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+var str = "Hello, playground"
+let myName = "mohammed almesri"
+
+var hobby1 =  "volleyball"
+var hobby2 = "reading "
+var age =  17
+var hight =  185.3
+print(" my name is", myName)
+print(" my hobby1 is", hobby1)
+print( " my hobby2 is", hobby2)
+print( " my age is", age)
